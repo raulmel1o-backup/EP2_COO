fa47b4ee56cd4bc095b8f8bdad5388244427c628
oiapokxui <enrique.emanuel@usp.br>
2021-07-31 22:29:36 -0300

    Refactored DifferentDatesException

--- src/main/infra/exception/DifferentDatesException.java
+++ src/main/infra/exception/DifferentDatesException.java
@@ -1,7 +1,10 @@
 package main.infra.exception;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class DifferentDatesException extends RuntimeException {
-    public DifferentDatesException(final String message) {
-        super(message);
+    public DifferentDatesException(final LocalDate dateA, final LocalDate dateB) {
+        super(dateA + " is not equal to " + dateB);
     }
 }
